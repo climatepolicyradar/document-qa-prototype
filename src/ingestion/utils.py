13cdd48a8301ba6dd975fac870d493cc62c40205
Henry Franks <hpwfranks@googlemail.com>
2024-08-13 13:51:46 +0100

    More ripping out

--- src/ingestion/utils.py
+++ src/ingestion/utils.py
@@ -1,10 +1,4 @@
 import json
-from typing import cast
-
-from chromadb.api.types import EmbeddingFunction, Embeddings
-from chromadb import Documents
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
-
 
 BLOCK_SEPARATOR = "\n\n----------------\n\n"
 
@@ -16,19 +10,3 @@ def transform_metadata(metadata: dict) -> dict:
         elif not isinstance(value, (str, int, float, bool)):
             metadata[key] = str(value)
     return metadata
-
-
-class ChromaBGEEmbeddingFunction(EmbeddingFunction):
-    """
-    A typing fix for the langchain embedder to make it compatible with Chroma.
-
-    TODO: this seems to slow down the embedding process
-    """
-
-    def __init__(self, model_name: str):
-        self.model_name = model_name
-        self.embedder = HuggingFaceBgeEmbeddings(model_name=self.model_name)
-
-    def __call__(self, input: Documents) -> Embeddings:
-        """Generate embeddings for a list of Documents (strings)."""
-        return cast(Embeddings, self.embedder.embed_documents(input))
